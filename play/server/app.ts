70cb10f36cb2d04b3fbb139ae2b1df3b8632a729
sgk <3228891558@qq.com>
2024-06-12 22:23:34 +0800

    chore: modify some files

--- play/server/app.ts
+++ play/server/app.ts
@@ -1,4 +1,22 @@
 import { cpus, totalmem, freemem } from 'node:os'
+import { createServer } from 'node:http'
 
-console.log(cpus())
+// libuv 
+class Bottle {
+  private app = createServer((req, res)=>{
 
+  })
+
+  listenAndServe(address: string, callback?: ()=>void) {
+    const [ host, port ] = address.split(/:/)
+
+    this.app.listen(parseInt(port), host, callback)
+  }
+}
+
+
+const app = new Bottle()
+
+
+
+app.listenAndServe('0.0.0.0:8080')
